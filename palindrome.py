14b13afafc97c42bc77b4e7310b26bfd5b1cd9a7
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-05-12 08:13:24 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -1,16 +1,15 @@
 #1.
-str = "1234321"
+str = "12321"
 str2=str[::-1]
 if str2==str:
     print("yes")
 else:
     print("no")
 
-#2
+#2 if a integer is given typecast it into string😅
 str = "1234321"
 if str==str[::-1]:
     print("yes")
 else:
     print("no")
     
-# if a integer is given typecast it into string😅
