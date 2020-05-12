06fd8107b053b32f04fb842261255b6e602c7769
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-05-12 09:34:19 +0530

    Update get-median.py

--- get-median.py
+++ get-median.py
@@ -1,7 +1,9 @@
-arr1 = [1,3,5,7,9,11]
-arr2 = [2,4,6,8,10,12]
+arr1 = [5,7,13,26,39]
+arr2 = [18,24,35,40,56]
 if arr1[len(arr1)-1]< arr2[0] or  arr1[0]> arr2[len(arr2)-1]:
-pass # ye codition abhi lagani hai
+    pass
+#ye condition abhi lagani baki hai
+
 
 a = (len(arr1)+ len(arr2))%2
 b = (len(arr1)+ len(arr2))//2
@@ -10,7 +12,7 @@ i=0
 j=0
 k=0
 if a==0:
-    while i < len(arr1) and j < len(arr2) and k<c:
+    while i < len(arr1) and j < len(arr2) and k<=c:
         if arr1[i]<arr2[j]:
             i=i+1
             k=k+1
