274741ad415ebd624339dd47f2f06f7f7d5f5f91
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-05-12 08:09:57 +0530

    Create palindrome.py

---
+++ palindrome.py
@@ -0,0 +1,16 @@
+#1.
+str = "1234321"
+str2=str[::-1]
+if str2==str:
+    print("yes")
+else:
+    print("no")
+
+#2
+str = "1234321"
+if str==str[::-1]:
+    print("yes")
+else:
+    print("no")
+    
+# if a integer is given typecast it into string😅
