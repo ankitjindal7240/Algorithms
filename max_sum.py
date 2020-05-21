612bd0103d3f5e77a2ec4a91310d4cd140243e3d
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-05-21 18:36:56 +0530

    Add files via upload

--- max_sum.py
+++ max_sum.py
@@ -1,15 +1,31 @@
-arr=[-2,1,-3,4,-1,2,1,-5,4]
-sum_arr=[]
-for i in range (len(arr)):
-    sub_arr = []
-    for j  in range (i,len(arr)):
-        sub_arr.append(arr[j])
-        #print(sub_arr)
-        sum = 0
-        for k in range(len(sub_arr)):
-            sum = sum +sub_arr[k]
-        sum_arr.append(sum)
+import sys
+
+#arr = [-2, -3, 4, -1, -2, 1, 5, -3]
+#arr = [-2, -3, -4, -1]
+#arr = [1,2,3,4,5]
+#arr = [1,2,3,4,5,-99]
+#arr = [1,2,3,4,5,-99,199]
+#arr = [1,2,3,4,5,-99,99]
+arr = [299,1,2,3,4,5,-99,199]
+
+max_sum = -sys.maxsize - 1
+print(max_sum)
+curr_sum = 0
+start_index =0
+j=0
+for i in range(len(arr)):
+    curr_sum = curr_sum + arr[i]
+    # print(curr_sum)
+
+    if curr_sum > max_sum:
+        max_sum = curr_sum
+        end_index = i
+        start_index=j
+    if curr_sum < 0:
+        curr_sum = 0
+        j= i+1
 
 
-#print(sum_arr)
-print(max(sum_arr))
+print("max giving sub array = ",arr[start_index:end_index+1])
+print(max_sum)
+
