0a42b9e3524d5fb47b3fd511234a10e48a00d00a
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-06-01 17:23:36 +0530

    Add files via upload

---
+++ same_element_2.py
@@ -0,0 +1,34 @@
+def binary_search_first(arr,l,r,x):
+    if r>=l:
+        mid = l + (r - l) // 2
+        if arr[mid]==x:
+            if arr[mid]!=arr[mid-1]:
+                return mid
+            else:
+                return binary_search_first(arr,l,mid-1,x)
+        elif arr[mid] > x:
+            return binary_search_first(arr, l, mid - 1, x)
+        else:
+            return binary_search_first(arr, mid + 1, r, x)
+    else:
+        return -1
+
+def binary_search_last(arr,l,r,x):
+    if arr[-1]==x:
+        return len(arr)-1
+    if r>=l:
+        mid = l + (r - l) // 2
+        if arr[mid]==x:
+            if arr[mid]!=arr[mid+1]:
+                return mid
+            else:
+                return binary_search_last(arr,l,mid-1,x)
+        elif arr[mid] > x:
+            return binary_search_last(arr, l, mid - 1, x)
+        else:
+            return binary_search_last(arr, mid + 1, r, x)
+    else:
+        return -1
+arr = [0,0,0,0,2,5,5,5,6,6,8,8,9,9,9]
+for i in range(10):
+    print(binary_search_first(arr,0,len(arr)-1,i),binary_search_last(arr,0,len(arr)-1,i))
