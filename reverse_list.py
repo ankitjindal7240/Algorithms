2ab852c62b3e16e996492a8e7e1c41f77700fd69
Ankit Jindal <59410552+ankitjindal7240@users.noreply.github.com>
2020-07-20 13:22:37 +0530

    Add files via upload

--- reverse_list.py
+++ reverse_list.py
@@ -33,75 +33,17 @@ l1=create_list_using_array(l1,a1)
 
 # main
 
-def reverse_list(l1):
-    l1=l1.head
-    temp1=l1.next           #2
-    if temp1 is None:
-        return
-    temp2=temp1.next        #3
-    l1.next=None            # 1.next =NONE
-    while temp2:
-        temp1.next=l1       #2.next=1
-        l1=temp1            #linklist starts fron 1
-        temp1=temp2
-        temp2=temp2.next
-    temp1.next=l1           #for last digit
-    l1=temp1
-    temp1=temp2
-    result=linklist()
-    result.head=l1
-    return result
-l= reverse_list(l1)
-print_list(l)
+def reverse_using_recursion(prev, current):
+    if current is None:
+        return prev
+    next=current.next
 
-
-
-
-# dry run
-
-# temp=l1 #1
-# temp2=temp.next#5
-# temp3=temp2.next #9
-# l1.next.next=l1#5->1
-# l1=l1.next
-# temp=temp2
-#
-# temp=temp3.next
-# temp3.next=l1
-# l1=temp3
-#
-# temp2=temp.next
-# temp.next=l1
-# l1=temp
-#
-# temp=temp2.next
-# temp2.next=l1
-# l1=temp2
-#
-# temp=l1
-# print(temp.data)
-# while temp!=None:
-#     print(temp.data)
-#     temp=temp.next
-
-
-
-# temp.next=l1
-# l1=temp
-# temp=temp2
-# temp2=temp2.next
-#
-#
-# temp.next=l1
-# l1=temp
-# temp=temp2
-# temp2=temp2.next
-#
-# temp.next=l1
-# l1=temp
-# temp=temp2
-# temp2=temp2.next
-#
-# temp.next=l1
-# l1=temp
-# temp=temp2
+    current.next=prev
+    prev=current
+    current=next
+    if next:
+        next=next.next
+    return reverse_using_recursion(prev, current)
+l2=linklist()
+l2.head=reverse_using_recursion(None, l1.head)
+print_list(l2)
